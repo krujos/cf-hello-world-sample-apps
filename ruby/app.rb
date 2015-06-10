900eb04d8989a8d67d48f5b40163d424efd6fd34
krujos <jkruck@pivotal.io>
2015-06-09 23:55:34 -0400

    Exceptions

--- ruby/app.rb
+++ ruby/app.rb
@@ -1,7 +1,30 @@
 require 'sinatra'
 
+def foo()
+  begin
+    raise Exception 'i am logging an exception'
+  rescue
+    puts $!, $@
+  end	
+  raise ArgumentError, 'Jacked' 
+end 
+
 get '/' do 
   "Hello from ruby!"
 end
 
+get '/log' do
+  begin
+    raise 'i am logging an exception'
+  rescue
+    puts $!, $@
+  end	
+end
 
+get '/throw' do
+  raise ArgumentError, 'Jacked' 
+end
+
+get '/foo' do
+  foo()
+end
